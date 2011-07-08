264247472f58eb00a247dbef6bf6be98c6735f62
Joseph McLean <mclean.joe@gmail.com>
2011-07-08 14:50:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpongeBuild::Application.config.secret_token = '1d6406f3d63863606d261d19abf241986dd5d06386e51ea1a48c8a2c179556a3f0e9b0c4b65d2bcd534514c1531914f1a20394249ee8968712811d8a4bd4fe0f'

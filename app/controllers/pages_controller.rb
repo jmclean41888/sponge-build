867d8caf188848592e803ec8417653639f2559f3
Joseph McLean <mclean.joe@gmail.com>
2011-07-11 00:34:25 -0400

    pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
 
   def build
   end
+  
+  def video
+  end
+  
+  def lead_generation
+  end
+  
+  def coupons
+  end
+  
+  def social_media
+  end
 
 end

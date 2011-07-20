943c1c8d94475e8d1a3511d95378280bb62aa021
Joseph McLean <mclean.joe@gmail.com>
2011-07-20 18:03:51 -0400

    most pages added, code refactored into layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,23 +4,31 @@ class PagesController < ApplicationController
   end
   
   def video
-  	@category = :feature
+  	@current_page_category = :feature
   end
   
   def lead_generation
-    @category = :feature
+    @current_page_category = :feature
   end
   
   def coupons
-    @category = :feature
+    @current_page_category = :feature
   end
   
   def social_media
-    @category = :feature
+    @current_page_category = :feature
   end
   
   def resize
-  	@current_page_category = "build"
+  	@current_page_category = :build
+  end
+  
+  def third_party
+  	@current_page_category = :trafficking
+  end
+  
+  def site_direct
+    @current_page_category = :trafficking
   end
 
 end
